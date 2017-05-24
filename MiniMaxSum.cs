45af7ffe1b9bb42105a203986d4b8890f9502e05
Koulick Sankar Paul <koulick89@gmail.com>
2017-05-24 18:41:10 -0500

    Mini Max Sum
    https://www.hackerrank.com/challenges/mini-max-sum

---
+++ MiniMaxSum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        string[] arr_temp = Console.ReadLine().Split(' ');
+        int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+        
+        long[] sum = new long[arr.Length];
+        long temp = 0;
+
+        for (int i = 0; i < arr.Length; i++) //arr.Length will be 5
+        {
+            temp += arr[i];
+        }
+        for (int j = 0; j < arr.Length; j++)
+        {
+            sum[j] = temp - arr[j];
+        }
+        Array.Sort(sum);
+        Console.Write(sum[0]);
+        Console.Write(" ");
+        Console.Write(sum[arr.Length - 1]);
+        Console.ReadKey();
+    }
+}
