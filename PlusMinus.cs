bd4f0c2004b9d037b45a6fa29d0c3596c3be7a8b
Koulick Sankar Paul <koulick89@gmail.com>
2017-05-24 18:41:48 -0500

    Plus Minus
    https://www.hackerrank.com/challenges/plus-minus

---
+++ PlusMinus.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] arr_temp = Console.ReadLine().Split(' ');
+        int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+        decimal positiveNumbers = 0, negativeNumbers = 0, zeroNumbers = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] > 0)
+            {
+                positiveNumbers += 1;
+            }
+            else if (arr[i] < 0)
+            {
+                negativeNumbers += 1;
+            }
+            else
+            {
+                zeroNumbers += 1;
+            }
+        }
+
+        Console.WriteLine((positiveNumbers / n).ToString("N6"));
+        Console.WriteLine((negativeNumbers / n).ToString("N6"));
+        Console.WriteLine((zeroNumbers / n).ToString("N6"));
+        Console.ReadKey();
+    }
+}
